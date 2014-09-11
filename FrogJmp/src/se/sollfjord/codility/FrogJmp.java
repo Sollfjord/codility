5e4d58774463084d32cfc56c227f79796cd8b351
Sollfjord <sollfjord@laptop>
2014-09-11 20:20:03 +0200

    Count minimal number of jumps from position X to Y.

---
+++ FrogJmp/src/se/sollfjord/codility/FrogJmp.java
@@ -0,0 +1,14 @@
+package se.sollfjord.codility;
+
+public class FrogJmp {
+	
+    public int solution(int X, int Y, int D) {
+        
+        
+        if((Y - X) % D == 0)
+        return (Y - X)/D;
+        else 
+        return (Y - X)/D +1;
+        
+    }
+}
